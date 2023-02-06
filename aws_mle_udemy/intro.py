6a0ad4d8eabe5900c5a785de14d105c864f4b66f
Christian Tillich <christian.tillich@WIL-LAP-DEV33.local>
2023-02-06 10:14:25 -0600

    Adding course intro.

---
+++ aws_mle_udemy/intro.py
@@ -0,0 +1,70 @@
+#%% [markdown]
+"""
+# Objective
+
+### Notes on Udemy
+
+Some useful features to remember:
+
+1. There's a Q&A section. You can post a question and browse questions that 
+    others have asked
+1. There's a speed function, you can slow down the lecture
+1. Probably want to turn on subtitles too. 
+
+### Course Outline
+
+1. Data Engineering (20%)
+    1. AWS Objectives
+        1. Create Data Repositories
+        1. Identify and Implement Ingestion Solutions
+        1. Identify and Implement Transformation Solutions
+    1. Course Materials
+        1. Storage - S3, Dynamo
+        1. Transformations - Glue, ETL
+        1. Streaming - Kinesis, Kinesis Video
+        1. Workflows - Data Pipelines, AWS Batch, Step Functions
+1. Exploratory Data Analysis (24%)
+    1. AWS Objectives
+        1. Sanitize and Prepare Data
+        1. Feature Engineering 
+        1. Analyze and Visualize
+    1. Course Materials
+        1. Data Science - scikit learn, distributions, trends
+        1. Analysis - Athena, Quicksight, EMR, Spark
+        1. Feature Engineering - Imputation, Outliers, Binning, Log Trans, One-Hot,
+            Scaling & Norms. 
+1. Modeling (36%)
+    1. AWS Objectives
+        1. Framing Business Problems as ML Problems
+        1. Select the Right Model
+        1. Training
+        1. Hyperparameter Optimization
+        1. Evaluation
+    1. Course Materials
+        1. Deep Learning - MLPs, CNNs, RNNs, Tuning Specifics
+        1. Sagemaker - Architecture, Built in Algos, AutoML, Spark Integration
+        1. High-Level AI Services - Comprehend, Translatte, Polly, Transcribe,
+            Lex, Rekognition, Personalize/Forecast/Textract, DeepLens
+        1. Evaluation and Tuning - Confusion Matrix, RMSE, Precision Recall, F1, 
+            ROC/AUC
+1. Implementation and Operations
+    1. AWS Objectives
+        1. Build for Performance, Availabiility, Scalability, Resiliency, and 
+            Fault Tolerance
+        1. Recommend the right service and features. 
+        1. Apply good security practices. 
+        1. Deploy and Operationalize Solutions
+    1. Course Materials
+        1. Sagemaker - Containers, Security, Instances, A/B Testing, Tensorflow, 
+            Neo and GreenGrass, Pipes, Elastic Inference, Inference Pipelines. 
+1. Take the Test
+
+### Course Materials
+
+[Course Materials](https://dw9ne0o7jcasn.cloudfront.net/AWSMachineLearning/AWSMachineLearning.zip)
+[Course Slides](http://media.sundog-soft.com/AWSMachineLearning/AWSCertifiedMLSlides.pdf)
+
+Remember that following along with an AWS console _will_ incur charges, as AWS
+services are not free. The course notes that on average they spend about $5 per
+week to follow along. 
+"""
